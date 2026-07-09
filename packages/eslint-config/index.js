dd59d76d8caf33d6559c1d6b83a0130ac5dacd5c
Julius Pahama <pahamajulius@gmail.com>
2026-07-10 02:47:39 +0800

    feat(eslint-config): Update ESLint configuration and versioning

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,29 +1,49 @@
-import js from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import onlyWarn from 'eslint-plugin-only-warn';
+import prettierPlugin from 'eslint-plugin-prettier';
+import turboPlugin from 'eslint-plugin-turbo';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 /**
  * A shared ESLint configuration for the repository.
  */
 export const config = defineConfig(
-  globalIgnores(["dist/**"]),
+  globalIgnores(['dist/**']),
   js.configs.recommended,
   eslintConfigPrettier,
   tseslint.configs.recommended,
   {
     plugins: {
+      prettier: prettierPlugin,
       turbo: turboPlugin,
     },
     rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+      'turbo/no-undeclared-env-vars': 'warn',
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto',
+          printWidth: 100,
+          tabWidth: 2,
+          singleQuote: true,
+          semi: true,
+          trailingComma: 'es5',
+        },
+      ],
     },
   },
   {
     plugins: {
       onlyWarn,
+    },
+  },
+  {
+    files: ['*.cjs', '*.js'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-undef': 'off',
     },
   }
 );
