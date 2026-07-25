93372359097e4c4fea1e8d18ed271805dd3b1b2f
Jeius <pahamajulius@gmail.com>
2026-07-26 05:51:16 +0800

    fix(frontend): add Netlify plugin to Vite configuration

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
@@ -10,5 +11,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
-})
+  plugins: [tailwindcss(), tanstackStart(), viteReact(), netlify()],
+});
