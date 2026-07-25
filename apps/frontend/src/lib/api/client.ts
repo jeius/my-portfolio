e0162e8ca8b696a7fca7b71f14d79a242062310d
Jeius <pahamajulius@gmail.com>
2026-07-26 06:54:38 +0800

    fix(frontend): trim API_URL and append '/api' to baseURL in PayloadSDK initialization

--- apps/frontend/src/lib/api/client.ts
+++ apps/frontend/src/lib/api/client.ts
@@ -3,5 +3,5 @@ import { Config } from '@jeius-portfolio/types/payload-types';
 import { API_URL } from '~/constants/env';
 
 export const apiClient = new PayloadSDK<Config>({
-  baseURL: API_URL,
+  baseURL: API_URL.trim() + '/api',
 })
